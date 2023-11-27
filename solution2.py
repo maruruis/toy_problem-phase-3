132742067142423a555896cd4c70b18e033de951
Ian Kimani <ianmwng@gmail.com>
2023-11-27 10:40:00 +0300

    complete assingment

--- solution2.py
+++ solution2.py
@@ -12,6 +12,5 @@ def check_positives(a, b, c):
     return positive_count == 2
 
 print(check_positives(8, 4, -1))  # Output: True
-print(check_positives(-7, 5, 3))  # Output: True
 print(check_positives(-5, 3, 0))  # Output: False
 print(check_positives(4, 2, 10))  # Output: False
